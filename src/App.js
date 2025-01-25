656a11c24e6585f22b1aa254284f16d509248df2
Jcastanodev <jcastanodev@gmail.com>
2025-01-25 14:16:00 -0500

    git user page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  console.log("APP", process.env.REACT_APP_URL);
+  console.log("PUBLIC_URL", process.env.PUBLIC_URL);
+  console.log("REACT_APP_URL", process.env.REACT_APP_URL);
+  console.log("REACT_APP_PUBLIC_URL", process.env.REACT_APP_PUBLIC_URL);
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Trepidantes <code>on construction</code> v0.0.1
+            Trepidantes <code>on construction</code> v0.0.2
           </p>
           <a
             className="App-link"
