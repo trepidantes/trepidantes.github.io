718d41f5e5e8846b36fbeaf2839a920a7d5f915a
Jcastanodev <jcastanodev@gmail.com>
2025-01-25 13:23:22 -0500

    user page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trepidantes <code>on construction</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://trepidantes.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to Trepidantes
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Trepidantes <code>on construction</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://trepidantes.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to Trepidantes
+          </a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
