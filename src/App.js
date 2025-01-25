942bae8d88144649674fa481d7b542ada8f70458
Jcastanodev <jcastanodev@gmail.com>
2025-01-25 14:35:12 -0500

    base url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
+let basename = document.querySelector('base')?.getAttribute('href') ?? '/';
+if (basename === '' || basename === ' ') {
+  basename = '/';
+}
+
 function App() {
   console.log("PUBLIC_URL", process.env.PUBLIC_URL);
   console.log("REACT_APP_URL", process.env.REACT_APP_URL);
   console.log("REACT_APP_PUBLIC_URL", process.env.REACT_APP_PUBLIC_URL);
+  console.log("basename", basename);
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={basename}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
